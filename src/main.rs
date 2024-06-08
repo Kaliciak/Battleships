234d1ec48a63d9020ab5580dc71c816dc4446691
Piotr Kaliciak <piotr-kaliciak@wp.pl>
2024-06-08 19:05:51 +0200

    Add an option to generate keys

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 use battleships::{gui::cli::run_cli_gui, logic::run_logic_with_gui};
+use clap::{Parser, Subcommand};
 
 fn main() {
-    run_logic_with_gui(run_cli_gui);
-    // battleships::circuit::board_declaration_circuit::generate_keys();
-    // battleships::circuit::field_declaration_circuit::generate_keys();
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Command::GenerateKeys) => {
+            battleships::circuit::board_declaration_circuit::generate_keys();
+            battleships::circuit::field_declaration_circuit::generate_keys();
+        }
+        None => {
+            run_logic_with_gui(run_cli_gui);
+        }
+    }
     println!("Następna stacja: Łódź Fabryczna")
-    // generate_keys();
+}
+
+#[derive(Debug, Parser)]
+#[clap(name = "cli", version)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    GenerateKeys
 }
