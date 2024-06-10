c96fe85885174f96ab83432103fed544ca96a20e
Agata Margas <agata.margas@student.uj.edu.pl>
2024-06-11 00:03:33 +0200

    Add command-line option for starting GUI or CLI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use battleships::{gui::run_gui, logic::run_logic_with_ui, ui::cli::run_cli_ui};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Commands {
+    #[command(subcommand)]
+    ui: Option<UI>
+}
+
+#[derive(Subcommand)]
+enum UI {
+    Gui,
+    Cli
+}
 
 fn main() {
-    // run_logic_with_ui(run_cli_ui);
-    run_logic_with_ui(run_gui);
+    let args = Commands::parse();
+    match args.ui {
+        Some(UI::Gui) => run_logic_with_ui(run_gui),
+        Some(UI::Cli) => run_logic_with_ui(run_cli_ui),
+        None => run_logic_with_ui(run_gui)
+    }
     // battleships::circuit::board_declaration_circuit::generate_keys();
     // battleships::circuit::field_declaration_circuit::generate_keys();
     println!("Następna stacja: Łódź Fabryczna")
